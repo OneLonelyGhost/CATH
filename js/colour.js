e653b40c40187e39b1afe772429cde74335c046e
Charlie <retrotommo@gmail.com>
2023-11-16 15:30:46 +0000

    map addition

--- js/colour.js
+++ js/colour.js
@@ -3,11 +3,13 @@ function changeColors(className, bgColor, textColor) {
   
   for (var i = 0; i < elements.length; i++) {
     elements[i].addEventListener('mouseover', function() {
+      this.style.transition = 'background-color 0.5s ease, color 0.5s ease';
       this.style.backgroundColor = bgColor;
       this.style.color = textColor;
     });
     
     elements[i].addEventListener('mouseout', function() {
+      this.style.transition = 'background-color 0.5s ease, color 0.5s ease';
       this.style.backgroundColor = '';
       this.style.color = '';
     });
